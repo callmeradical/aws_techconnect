31bc0822622c431e2cdc894f9f31e1fd46d3d8e9
Lars Cromley <lars@paniksrvr.info>
2015-10-26 03:10:47 -0400

    fixed finally I can go to bed

--- scripts/set_env.rb
+++ scripts/set_env.rb
@@ -6,7 +6,7 @@ doc = `curl #{url}/#{role}/`
 creds = JSON.parse(doc)
 File.open('creds', 'w') do |file|
   file.write("export AWS_KEY=\"#{creds['AccessKeyId']}\"\n")
-  file.write("export AWS_SECRET=\"#{creds['SecretAccessKey']}\"")
-  file.write("export BUCKET=\"s3://2wtechconnectphoto\"")
+  file.write("export AWS_SECRET=\"#{creds['SecretAccessKey']}\"\n")
+  file.write("export BUCKET=\"s3://2wtechconnectphoto\"\n")
   file.write("export SITE=\"s3://2wtechconnectsite\"")
 end
